8bbefe3cfdc1181fd3a2de93c1c9757e7480ce19
Roman Metlinskyi <gmail@metlinskyi.com>
2026-05-03 16:19:14 +0200

    rename

--- Api/Handlers/Register.cs
+++ Api/Handlers/Register.cs
@@ -6,7 +6,7 @@ public static class Register
 {
     public static void EndpointsCollection(this IHappy.Middleware middleware)
     {
-        middleware.Register<LoginRequest, LoginResponse, LoginHandler>(AppJsonSerializerContext.Default);
-        middleware.Register<LogoutRequest, LogoutHandler>(AppJsonSerializerContext.Default);
+        middleware.Register<LoginRequest, LoginResponse, LoginEndpoint>(AppJsonSerializerContext.Default);
+        middleware.Register<LogoutRequest, LogoutEndpoint>(AppJsonSerializerContext.Default);
     }
 }
